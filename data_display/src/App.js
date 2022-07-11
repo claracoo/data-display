77b027effe6fd3be88bbc79cede4ee86beb3f51e
Clara Cook <claracoo@buffalo.edu>
2022-07-11 14:44:27 -0400

    updated firebase

--- data_display/src/App.js
+++ data_display/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 
 // import firebase from 'firebase/app';
@@ -16,6 +16,12 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 const fs = require('browserify-fs');
 var data = require("./articles.json");
+var apple = require("./apple.json");
+var google = require("./google.json");
+var meta = require("./meta.json");
+var tesla = require("./tesla.json");
+var model = require("./NLPW-BFWC.json");
+var apple_json = require("./AAPL.json");
 
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('5db6ce66de334330a7a6f964e1726075', { corsProxyUrl: 'https://cors-anywhere.herokuapp.com/' });
@@ -36,54 +42,93 @@ firebase.initializeApp({
 const auth = firebase.auth()
 const firestore = firebase.firestore() 
 
+function addModelStats(model_name, model_json) {
+  for (let elem of model_json) {
+    firestore.collection("models").doc(model_name).collection("stats").add({
+      accuracy: elem.accuracy,
+      precision: elem.precision,
+      recall: elem.recall,
+      f1: elem.f1,
+      date: elem.date,
+      measure_type: "dhc"
+  });
+}
+}
+function addEntityStats(entity_name, entity_json) {
+  for (let elem of entity_json) {
+    firestore.collection("entities").doc(entity_name).collection("stats").add({
+      date: elem.date,
+      tagging_volume: elem.tagging_volume,
+      confidence_score: elem.confidence_score,
+      high_rel: elem.high_rel,
+      med_rel: elem.med_rel,
+      low_rel: elem.low_rel,
+      readership: elem.readership
+  });
+}
+}
+
 
 
 function App() {
+  const [articleNum, setArticleNum] = useState(71)
   const [user] = useAuthState(auth);
-//   let object = {
-//     id: 123,
-//     name: "Anj",
-//     age: 28,
-//     registered: true
-// }
-// let data = JSON.stringify(object)
-// fs.writeFile('./articles.json', data, (err) => {
-//   console.log(data)
-//       if (err) {
-//           throw err;
-//       }
-//       console.log("JSON data is saved.");
-//   });
-// console.log("yoooo", data["articles"])
-  newsapi.v2.everything({
-    sources: 'bbc-news,the-verge',
-    domains: 'bbc.co.uk,techcrunch.com',
-    language: 'en',
-    sortBy: 'relevancy',
-    q: 'facebook',
-    page: 6
-  }).then(response => {
-    console.log(response);
-    let data = JSON.stringify(response);
-    console.log(data)
-    fs.writeFile('./articles.json', data, (err) => {
-      if (err) {
-          throw err;
-      }
-      console.log("JSON data is saved.");
+  
+  useEffect(() => {
+    firestore.collection("messages").doc("top_level")
+  .get()
+  .then(function(doc) {
+    if (doc.exists) {
+      console.log("Document data:", doc.data());
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+      firestore.collection("messages").doc("top_level").set({
+        NLPWBFWC_articles: 72,
+        AAPL_articles: 89
+    })    
+    }
+    
+  }).catch(function(error) {
+    console.log("Error getting document:", error);
   });
-  });
+  })
+
+  const handleArticleIncrease = ({increase}) => {
+    console.log(increase)
+    setArticleNum(articleNum + increase)
+  }
+  // newsapi.v2.everything({
+  //   sources: 'bbc-news,the-verge',
+  //   domains: 'bbc.co.uk,techcrunch.com',
+  //   language: 'en',
+  //   sortBy: 'relevancy',
+  //   q: 'facebook',
+  //   page: 6
+  // }).then(response => {
+  //   console.log(response);
+  //   let data = JSON.stringify(response);
+  //   console.log(data)
+  //   fs.writeFile('./articles.json', data, (err) => {
+  //     if (err) {
+  //         throw err;
+  //     }
+  //     console.log("JSON data is saved.");
+  // });
+  // });
   
 
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        {/* <h1>⚛️🔥💬</h1> */}
         <SignOut />
       </header>
-
+    <section>
+      <p><span>{articleNum}</span> articles</p>
+    </section>
       <section>
-        {user ? <ChatRoom /> : <SignIn />}
+        {user ? <AddAlert onClick={handleArticleIncrease}/> : <SignIn />}
       </section>
 
     </div>
@@ -100,7 +145,7 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      {/* <p>Do not violate the community guidelines or you will be banned for life!</p> */}
     </>
   )
 
@@ -113,7 +158,7 @@ function SignOut() {
 }
 
 
-function ChatRoom() {
+function AddAlert(props) {
   const dummy = useRef();
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
@@ -139,19 +184,22 @@ function ChatRoom() {
   return (<>
     <main>
 
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-
+      {/* {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)} */}
+      <button onClick={(e) => {
+            e.preventDefault();
+            props.onClick({ increase: 1});
+          }}>Send Articles</button>
       <span ref={dummy}></span>
 
     </main>
 
-    <form onSubmit={sendMessage}>
+    {/* <form onSubmit={sendMessage}>
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
       <button type="submit" disabled={!formValue}>🕊️</button>
 
-    </form>
+    </form> */}
   </>)
 }
 
